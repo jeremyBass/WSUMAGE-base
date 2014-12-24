79a0398e116109f16d25996073d816935459a388
Jeremy Bass <jeremybass@cableone.net>
2014-12-24 04:23:57 -0800

    wrong format

--- provision/salt/stage/vagrant/scripts/install-config.php
+++ provision/salt/stage/vagrant/scripts/install-config.php
@@ -1,10 +1,10 @@
 <?php
-define('BASEURL', '{{ magento.baseurl|lower }}');
-define('SAMPLE_STORE', '{{ magento.sample_stores|lower }}');
+define('BASEURL', '{{ magento[baseurl]|lower }}');
+define('SAMPLE_STORE', '{{ magento[sample_stores]|lower }}');
 
-define('ADMIN_URL', 'http://store.admin.{{ magento.baseurl|lower }}/');
-define('UNSECURE_BASE_URL', 'http://store.admin.{{ magento.baseurl|lower }}/');
-define('SECURE_BASE_URL', 'http://store.admin.{{ magento.baseurl|lower }}/');
+define('ADMIN_URL', 'http://store.admin.{{ magento[baseurl]|lower }}/');
+define('UNSECURE_BASE_URL', 'http://store.admin.{{ magento[baseurl]|lower }}/');
+define('SECURE_BASE_URL', 'http://store.admin.{{ magento[baseurl]|lower }}/');
 
 
 
