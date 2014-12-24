daf4d54e9515f93cd6909cb02ebfd7fdd72374f8
Jeremy Bass <jeremybass@cableone.net>
2014-12-24 03:14:06 -0800

    the defined items are supost to be strings

--- provision/salt/stage/vagrant/scripts/install-config.php
+++ provision/salt/stage/vagrant/scripts/install-config.php
@@ -1,13 +1,6 @@
- /*   isLocal:  vars.isLocal 
-      magento:  magento 
-      database:  database 
-      project:  project 
-      saltenv:  saltenv 
-*/
-
 <?php
-define('BASEURL', {{ magento.baseurl|lower }});
-define('SAMPLE_STORE', {{ magento.sample_stores|lower }} );
+define('BASEURL', '{{ magento.baseurl|lower }}');
+define('SAMPLE_STORE', '{{ magento.sample_stores|lower }}');
 
 
 
