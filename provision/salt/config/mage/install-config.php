a240bcb17035b9062a981d2e494c7fd7764436bc
Jeremy Bass <jeremybass@cableone.net>
2015-12-05 12:58:39 -0800

    match up the new names used in the mage object

--- provision/salt/config/mage/install-config.php
+++ provision/salt/config/mage/install-config.php
@@ -1,5 +1,5 @@
 <?php
-define('BASEURL', '{{ magento['baseurl']|lower }}');
+define('BASEURL', '{{ magento['base-url']|lower }}');
 define('SAMPLE_STORE', '{{ magento['sample_stores']|lower }}');
 define('SETTINGS_INSTALLED', '{{ settings_installed }}');
 define('OVERLOAD_SETTINGS', '{{ magento['overload_settings']|lower }}'); 
@@ -7,7 +7,7 @@ define('OVERLOAD_SETTINGS', '{{ magento['overload_settings']|lower }}');
 
 define('UNSECURE_BASE_URL', 'http://{{ magento['url']|lower }}/');
 define('SECURE_BASE_URL', 'https://{{ magento['url']|lower }}/');
-define('ADMIN_URL', 'http://{{ magento['admin_url']|lower }}/');
+define('ADMIN_URL', 'http://{{ magento['url']|lower }}/{{ magento['backend-frontname'] }}/');
 
 $_GLOBAL['STORES'] = array(
 {% for ext_key, ext_val in stores|dictsort %}
