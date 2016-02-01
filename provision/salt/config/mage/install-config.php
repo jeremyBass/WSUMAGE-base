8f6912c563ee98adda78ff6ac271169fd96b9443
Jeremy Bass <jeremybass@cableone.net>
2016-02-01 11:49:41 -0800

    - to _ conversion parts missed

--- provision/salt/config/mage/install-config.php
+++ provision/salt/config/mage/install-config.php
@@ -1,13 +1,13 @@
 <?php
-define('BASEURL', '{{ magento['base-url']|lower }}');
+define('BASEURL', '{{ magento['base_url']|lower }}');
 define('SAMPLE_STORE', '{{ magento['sample_stores']|lower }}');
 define('SETTINGS_INSTALLED', '{{ settings_installed }}');
-define('OVERLOAD_SETTINGS', '{{ magento['overload_settings']|lower }}'); 
+define('OVERLOAD_SETTINGS', '{{ magento['overload_settings']|lower }}');
 
 
 define('UNSECURE_BASE_URL', 'http://{{ magento['url']|lower }}/');
 define('SECURE_BASE_URL', 'https://{{ magento['url']|lower }}/');
-define('ADMIN_URL', 'http://{{ magento['url']|lower }}/{{ magento['backend-frontname'] }}/');
+define('ADMIN_URL', 'http://{{ magento['url']|lower }}/{{ magento['backend_frontname'] }}/');
 
 $_GLOBAL['STORES'] = array(
 {% for ext_key, ext_val in stores|dictsort %}
